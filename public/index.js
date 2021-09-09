584345e7765636d455b75da808d5c55c568dc4b7
Jeremy Brown <jeremy.brown@proofgeist.com>
2021-09-09 09:12:44 -0400

    trying on terminal

--- public/index.js
+++ public/index.js
@@ -1,3 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style.css";
 import "regenerator-runtime/runtime.js";
+import c3 from "c3";
+import "c3/c3.min.css";
+
+window.loadChart = (obj) => {
+  const data = JSON.parse(obj);
+
+  var chart = c3.generate({
+    bindto: "#chart",
+    data: {
+      columns: data,
+    },
+  });
+};
